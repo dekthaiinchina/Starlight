ba9a1f74abba5d321a54dd630cfdd41abfb273a2
EvarinDev <evarin.dev@gmail.com>
2024-12-17 13:36:48 +0700

    fix: update typescript-eslint version and comment out autocomplete logic in music command

--- src/client/commands/music/play.ts
+++ src/client/commands/music/play.ts
@@ -5,45 +5,70 @@ export const PlayCommandOptions = {
 	search: createStringOption({
 		description: "[EN]: The song you want to play | [TH]: เพลงที่คุณต้องการเล่น",
 		required: true,
-		autocomplete: async (interaction: AutocompleteInteraction) => {
-			const { client } = interaction;
-			const song = interaction.getInput();
-			const res = await client.sakulink.search({
-				query: song || "Song",
-				source: "youtube",
-			});
+		// autocomplete: async (interaction: AutocompleteInteraction) => {
+		// 	const { client } = interaction;
+		// 	const song = interaction.getInput();
+		// 	const res = await client.sakulink.search({
+		// 		query: song,
+		// 	});
 
-			const songs: {
-				name: string;
-				value: string;
-			}[] = [];
-			if (res.loadType === "search") {
-				for (let i = 0; i < res.tracks.length; i++) {
-					const track = res.tracks[i];
-					songs.push({
-						name: track.title,
-						value: track.track,
-					});
-				}
-			}
-			if (songs.length > 0) {
-				await interaction
-					.respond(songs)
-					.then(() => {})
-					.catch(() => {});
-				songs.pop();
-			} else {
-				await interaction
-					.respond([
-						{
-							name: "No results found",
-							value: song
-						}
-					])
-					.then(() => {})
-					.catch(() => {});
-			}
-		},
+		// 	const songs: {
+		// 		name: string;
+		// 		value: string;
+		// 	}[] = [];
+		// 	switch (res.loadType) {
+		// 		case "playlist": {
+		// 			for (const track of res.tracks) {
+		// 				songs.push({
+		// 					name: track.title,
+		// 					value: track.uri,
+		// 				});
+		// 			}
+		// 			break;
+		// 		}
+		// 		case "track": {
+		// 			for (let i = 0; i < res.tracks.length; i++) {
+		// 				const track = res.tracks[i];
+		// 				songs.push({
+		// 					name: track.title,
+		// 					value: track.track,
+		// 				});
+		// 			}
+		// 			break;
+		// 		}
+		// 		case "search": {
+		// 			for (let i = 0; i < res.tracks.length; i++) {
+		// 				const track = res.tracks[i];
+		// 				songs.push({
+		// 					name: track.title,
+		// 					value: track.track,
+		// 				});
+		// 			}
+		// 			break;
+		// 		}
+		// 		default:
+		// 			break
+		// 	}
+
+		// 	if (songs.length > 0) {
+		// 		await interaction
+		// 			.respond(songs.map((song) => song))
+		// 			.then(() => client.logger.info(`Responded with ${songs.length} songs for ${song}`))
+		// 			.catch(() => { });
+		// 		return songs.pop();
+		// 	} else {
+		// 		await interaction
+		// 			.respond([
+		// 				{
+		// 					name: "No results found",
+		// 					value: song
+		// 				}
+		// 			])
+		// 			.then(() => client.logger.info(`Responded with no results found for ${song}`))
+		// 			.catch(() => { });
+		// 		return
+		// 	}
+		// },
 	}),
 	node: createStringOption({
 		description: "[EN]: The node you want to play the song | [TH]: โหนดที่คุณต้องการเล่นเพลง",
@@ -56,7 +81,7 @@ export const PlayCommandOptions = {
 				name: `${node.options.identifier} - ${node.stats.players} Players`,
 				value: node.options.identifier,
 			}));
-			return await interaction.respond(nodes).catch(() => {});
+			return await interaction.respond(nodes).catch(() => { });
 		},
 	}),
 };
